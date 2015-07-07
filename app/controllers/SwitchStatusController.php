5f2f996fb4573250495e88127929401166f6f8de
Dennis <dennis@e-diary.com>
2015-07-07 09:54:21 +0800

    Laravel Framework

---
+++ app/controllers/SwitchStatusController.php
@@ -0,0 +1,17 @@
+<?php
+
+class SwitchStatusController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Switch Controller
+	|--------------------------------------------------------------------------
+	|
+	*/
+
+	public function getSwitchStatus()
+	{
+		return 'Current Switches Status';
+	}
+
+}
